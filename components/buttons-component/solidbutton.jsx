e905da3f96a85ede26711fff74776b4d81b4d882
farruxuzbekistan <69811272+farruxuzbekistan@users.noreply.github.com>
2024-09-21 22:26:16 +0500

    style added

--- components/buttons-component/solidbutton.jsx
+++ components/buttons-component/solidbutton.jsx
@@ -13,7 +13,7 @@ const Button = ({
     <button
       onClick={onClick}
       style={{ borderWidth: 1.5, borderRadius: 4, ...styles }}
-      className={`bg-red-500 border-2 border-red-500 ${padding}  ${fontSize} ${fontWeight} ${furtherClasses}   duration-300 text-white hover:bg-white hover:text-red-500 transition-all`}
+      className={`bg-[#3d1a54] border-2 border-[#3d1a54] ${padding} ${fontSize} ${fontWeight} ${furtherClasses} duration-300 text-white hover:bg-white hover:text-[#3d1a54] transition-all`}
     >
       {content}
     </button>
